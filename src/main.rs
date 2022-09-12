2ecad49702f17310d8a3dd5916866148491f3e12
DurbeKK <k2.durbek@gmail.com>
2022-09-12 21:11:48 +0500

    sleep between requests

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,9 @@ pub mod types;
 
 use crate::instance::Instance;
 use dotenv::dotenv;
+use rand::{thread_rng, Rng};
+use std::thread::sleep;
+use std::time::Duration;
 
 fn main() {
     dotenv().ok();
@@ -14,9 +17,15 @@ fn main() {
 
     let instance = Instance::new(credentials.0.as_str(), credentials.1.as_str());
 
+    let mut rng = thread_rng();
+
     for group in instance.codes.keys() {
         println!("Scraping: {}", group);
         let timetable = instance.get_timetable(group);
         println!("{:#?}", timetable);
+
+        // sleep a random number of seconds between each request
+        // since we don't want to overwhelm wiut's servers.....right?
+        sleep(Duration::from_secs_f64(rng.gen_range(0.5..1.4142)))
     }
 }
